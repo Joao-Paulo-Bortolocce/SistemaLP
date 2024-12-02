5017c22b07d2f460444378049f9e1c6708968a03
Joao-Paulo-Bortolocce <bbortolocce@gmail.com>
2024-12-01 22:39:19 -0300

    Finalizando o redux de produto

--- src/componentes/Telas/formularios/CadastrosProduto.jsx
+++ src/componentes/Telas/formularios/CadastrosProduto.jsx
@@ -4,19 +4,28 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
-import { Spinner } from 'react-bootstrap';
+import { Alert,Spinner } from 'react-bootstrap';
 import { consultarCategoria } from '../../../servicos/servicoCategoria.js';
-import toast,{Toaster} from 'react-hot-toast';
-import { gravarProduto } from '../../../servicos/servicoProduto.js';
-import { alterarProduto } from '../../../servicos/servicoProduto.js';
+import toast, { Toaster } from 'react-hot-toast';
+import { useSelector, useDispatch } from 'react-redux';
+import { incluirProduto, atualizarProduto } from '../../../redux/produtoReducer.js'
+import ESTADO from '../../../redux/estados.js';
 
 
 export default function CadastroProduto(props) {
   //const [validated, setValidated] = useState(false);
   const [formValidado, setFormValidado] = useState(false);
   const [categorias, setCategorias] = useState([]);
-  const [temCategorias,setTemCategorias]= useState(false)
+  const [temCategorias, setTemCategorias] = useState(false);
+  const { estado, mensagem} = useSelector((state) => state.produto);
+  const despachante = useDispatch();
 
+  /*useEffect(()=>{
+    const dtVal= document.getElementById('dtValidade');
+    if(props.modoEdicao){
+      dtVal.value=props.produto.dtValidade.substr(0,10);
+    }
+  })*/
 
   useEffect(() => {
     consultarCategoria()
@@ -32,21 +41,35 @@ export default function CadastroProduto(props) {
         toast.error("Não foi possível carregar as categorias");
       });
   }, []);
-  
-  function selecionarCategoria(event){
-    props.setProduto({...props.produto, categoria:{
-      codigo:event.currentTarget.value
-    }})
+
+  function selecionarCategoria(event) {
+    props.setProduto({
+      ...props.produto, categoria: {
+        codigo: event.currentTarget.value
+      }
+    })
   }
 
+  function zeraProduto(){
+    props.setProduto({
+      codigo: 0,
+      descricao: "",
+      precoCusto: 0,
+      precoVenda: 0,
+      qtdEstoque: 0,
+      urlImagem: "",
+      dtValidade: "3000-12-30"
+    })
+  }
+  
   function manipularSubmissao(evento) {
     const form = evento.currentTarget;
     if (form.checkValidity()) {
       //Cadastrar o produto
       if (props.modoEdicao) {
         // const prods = props.listaDeProdutos.map((item) => {
-        //   if (item.codigo === props.produto.codigo) {
-        //     // item.descricao=props.produto.descricao;
+          //   if (item.codigo === props.produto.codigo) {
+            //     // item.descricao=props.produto.descricao;
         //     // item.qtdEstoque=props.produto.qtdEstoque;
         //     // item.precoCusto= props.produto.precoCusto;
         //     // item.precoVenda=props.produto.precoVenda;
@@ -55,36 +78,15 @@ export default function CadastroProduto(props) {
         //     return props.produto;
         //   }
         //   else {
-
+          
         //     console.log("Diferentes")
         //     return item;
         //   }
         // });
-        alterarProduto(props.produto).then((resultado)=>{
-            if(resultado.status){
-              props.setExibirTabela(true);
-              props.setModoEdicao(false);
-              props.setProduto({
-                codigo: 0,
-                descricao: "",
-                precoCusto: 0,
-                precoVenda: 0,
-                qtdEstoque: 0,
-                urlImagem: "",
-                dtValidade: "3000-12-30"
-              })
-            }
-            else{
-              toast.error(resultado.mensagem)
-          }
-        })
-      }
-      //Exibir a tabela com o produto incluido
-      else {
-
-        gravarProduto(props.produto).then((resultado)=>{
-          if(resultado.status){
+        /*alterarProduto(props.produto).then((resultado) => {
+          if (resultado.status) {
             props.setExibirTabela(true);
+            props.setModoEdicao(false);
             props.setProduto({
               codigo: 0,
               descricao: "",
@@ -93,12 +95,38 @@ export default function CadastroProduto(props) {
               qtdEstoque: 0,
               urlImagem: "",
               dtValidade: "3000-12-30"
-            })
+              })
+              }
+              else {
+                toast.error(resultado.mensagem)
+            }
+            })*/
+           
+           despachante(atualizarProduto(props.produto));
+            setTimeout(()=>{
+            props.setExibirTabela(true);
+            zeraProduto();
+
+            },5000)           
+
+      }
+      //Exibir a tabela com o produto incluido
+      else {
+
+        despachante(incluirProduto(props.produto));
+        setTimeout(()=>{
+          props.setExibirTabela(true);
+          zeraProduto();
+
+        },5000)
+        
+        /*gravarProduto(props.produto).then((resultado) => {
+          if (resultado.status) {
           }
-          else{
-              toast.error(resultado.mensagem)
+          else {
+            toast.error(resultado.mensagem)
           }
-        })
+        })*/
       }
 
 
@@ -128,148 +156,171 @@ export default function CadastroProduto(props) {
 
   //   setValidated(true);
   // };
+  if (estado === ESTADO.PENDENTE) {
+    return (
+      <div>
 
-
-  return (
-    <Form noValidate validated={formValidado} onSubmit={manipularSubmissao} className='container'>
-      <Row className="mb-4">
-        <Form.Group as={Col} md="4" >
-          <Form.Label>Código</Form.Label>
-          <Form.Control type="number" required id="codigo"
-            disabled={props.modoEdicao}
-            value={props.produto.codigo}
-            onChange={manipularMudanca}
-          />
-          <Form.Control.Feedback type="invalid">
-            Por-Favor informe o código do produto
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="4" >
-          <Form.Label>Descrição</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="Descrição"
-            id="descricao"
-            value={props.produto.descricao}
-            onChange={manipularMudanca}
-          />
-          <Form.Control.Feedback type='invalid'>Informe a Descrição do produto</Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="3" >
-          <Form.Label>Estoque</Form.Label>
-          <InputGroup hasValidation>
-            <Form.Control
-              type="number"
-              placeholder="estoque"
-              aria-describedby="inputGroupPrepend"
-              min="0"
-              required
-              id="qtdEstoque"
-              value={props.produto.qtdEstoque}
-              onChange={manipularMudanca}
-            />
-            <Form.Control.Feedback type="invalid">
-              Por-favor informe um valor para o estoque
-            </Form.Control.Feedback>
-          </InputGroup>
-        </Form.Group>
-      </Row>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="3" >
-          <Form.Label>Preço de custo</Form.Label>
-          <InputGroup hasValidation>
-            <InputGroup.Text id="inputGroupPrepend">R$</InputGroup.Text>
-            <Form.Control min="0" type='number' placeholder="Preço de custo" required
-              id="precoCusto"
-              value={props.produto.precoCusto}
-              onChange={manipularMudanca}
-            />
-            <Form.Control.Feedback type="invalid">
-              Por-favor informe o Preço de custo
-            </Form.Control.Feedback>
-          </InputGroup>
-        </Form.Group>
-        <Form.Group as={Col} md="3" >
-          <Form.Label>Preço de Venda</Form.Label>
-          <InputGroup hasValidation>
-            <InputGroup.Text id="inputGroupPrepend">R$</InputGroup.Text>
-            <Form.Control min="0" type="number" placeholder="Preço de venda" required
-              id="precoVenda"
-              value={props.produto.precoVenda}
-              onChange={manipularMudanca}
-            />
-            <Form.Control.Feedback type="invalid">
-              Por-favor informe o Preço de venda
-            </Form.Control.Feedback>
-          </InputGroup>
-        </Form.Group>
-      </Row>
-      <Row className="mb-3">
-        <Form.Group as={Col} md="3" >
-          <Form.Label>Data de validade</Form.Label>
-          <Form.Control type="date" required
-            id="dtValidade"
-            value={props.produto.dtValidade}
-            onChange={manipularMudanca}
-          />
-          <Form.Control.Feedback type="invalid">
-            Por-favor informe a data de validade
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="4" >
-          <Form.Label>Url da imagem</Form.Label>
-          <Form.Control type="text" required
-            id="urlImagem"
-            value={props.produto.urlImagem}
-            onChange={manipularMudanca}
-          />
-          <Form.Control.Feedback type="invalid">
-            Por-favor informe a url da imagem
-          </Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md={3}>
-          <Form.Label>Categoria:</Form.Label>
-          <Form.Select aria-label="Default select example" id="categoria" name="categoria" onChange={selecionarCategoria} value={props.produto.categoria.codigo}>
-            <option>Selecione uma categoria</option>
-            {// Criar em tempo de execução as categorias existentes no banco de dados
-              categorias.map((categoria) => {
-                return (
-                  <option value={categoria.codigo}>{categoria.descricao}</option>
-                )
-              })
-            }
-          </Form.Select>
-        </Form.Group>
-        <Form.Group as={Col} md={1}>
-         { !temCategorias? <Spinner animation="border" role="status">
-            <span className="visually-hidden">Loading...</span>
-          </Spinner>: ""}
-        </Form.Group>
-      </Row>
-      <Form.Group className="mb-3">
-        <Form.Check
-          required
-          label="Concordo com os termos de uso"
-          feedback="Você tem que concordar antes de finalizar o cadastro"
-          feedbackType="invalid"
-        />
-      </Form.Group>
-      <Row className='mt-2 mb-2'>
-        <Col md={1}>
-          <Button id="botao" disabled={!temCategorias} type="submit">{props.modoEdicao ? "Alterar" : "Cadastrar"}</Button>
-
-        </Col>
-        <Col md={{ offset: 1 }} >
+        <Spinner animation="border" role="status"></Spinner>
+        <Alert variant="primary">{mensagem}</Alert>
+      </div>
+    )
+  }
+  else
+    if (estado === ESTADO.ERRO) {
+      return (
+        <div>
+          <Alert variant="danger">{mensagem}</Alert>
           <Button onClick={() => {
-            props.setExibirTabela(true);
-            props.setModoEdicao(false);
-          }}>Voltar</Button>
+           props.setExibirTabela(true);
+           props.setModoEdicao(false);
+         }}>Voltar</Button>
+        </div>
+      )
+    }
+    else {
 
-        </Col>
-      </Row>
-      <Toaster position='top-right'/>
-    </Form>
-    
-  );
+
+      return (
+        <Form noValidate validated={formValidado} onSubmit={manipularSubmissao} className='container'>
+          <Row className="mb-4">
+            <Form.Group as={Col} md="4" >
+              <Form.Label>Código</Form.Label>
+              <Form.Control type="number" required id="codigo"
+                disabled={props.modoEdicao}
+                value={props.produto.codigo}
+                onChange={manipularMudanca}
+              />
+              <Form.Control.Feedback type="invalid">
+                Por-Favor informe o código do produto
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="4" >
+              <Form.Label>Descrição</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Descrição"
+                id="descricao"
+                value={props.produto.descricao}
+                onChange={manipularMudanca}
+              />
+              <Form.Control.Feedback type='invalid'>Informe a Descrição do produto</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="3" >
+              <Form.Label>Estoque</Form.Label>
+              <InputGroup hasValidation>
+                <Form.Control
+                  type="number"
+                  placeholder="estoque"
+                  aria-describedby="inputGroupPrepend"
+                  min="0"
+                  required
+                  id="qtdEstoque"
+                  value={props.produto.qtdEstoque}
+                  onChange={manipularMudanca}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Por-favor informe um valor para o estoque
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+          </Row>
+          <Row className="mb-3">
+            <Form.Group as={Col} md="3" >
+              <Form.Label>Preço de custo</Form.Label>
+              <InputGroup hasValidation>
+                <InputGroup.Text id="inputGroupPrepend">R$</InputGroup.Text>
+                <Form.Control min="0" type='number' placeholder="Preço de custo" required
+                  id="precoCusto"
+                  value={props.produto.precoCusto}
+                  onChange={manipularMudanca}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Por-favor informe o Preço de custo
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+            <Form.Group as={Col} md="3" >
+              <Form.Label>Preço de Venda</Form.Label>
+              <InputGroup hasValidation>
+                <InputGroup.Text id="inputGroupPrepend">R$</InputGroup.Text>
+                <Form.Control min="0" type="number" placeholder="Preço de venda" required
+                  id="precoVenda"
+                  value={props.produto.precoVenda}
+                  onChange={manipularMudanca}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Por-favor informe o Preço de venda
+                </Form.Control.Feedback>
+              </InputGroup>
+            </Form.Group>
+          </Row>
+          <Row className="mb-3">
+            <Form.Group as={Col} md="3" >
+              <Form.Label>Data de validade</Form.Label>
+              <Form.Control type="date" required
+                id="dtValidade"
+                onChange={manipularMudanca}
+                />
+              <Form.Control.Feedback type="invalid">
+                Por-favor informe a data de validade
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="4" >
+              <Form.Label>Url da imagem</Form.Label>
+              <Form.Control type="text" required
+                id="urlImagem"
+                value={props.produto.urlImagem}
+                onChange={manipularMudanca}
+              />
+              <Form.Control.Feedback type="invalid">
+                Por-favor informe a url da imagem
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md={3}>
+              <Form.Label>Categoria:</Form.Label>
+              <Form.Select aria-label="Default select example" id="categoria" name="categoria" onChange={selecionarCategoria} value={props.produto.categoria.codigo}>
+                <option>Selecione uma categoria</option>
+                {// Criar em tempo de execução as categorias existentes no banco de dados
+                  categorias.map((categoria) => {
+                    return (
+                      <option value={categoria.codigo}>{categoria.descricao}</option>
+                    )
+                  })
+                }
+              </Form.Select>
+            </Form.Group>
+            <Form.Group as={Col} md={1}>
+              {!temCategorias ? <Spinner animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </Spinner> : ""}
+            </Form.Group>
+          </Row>
+          <Form.Group className="mb-3">
+            <Form.Check
+              required
+              label="Concordo com os termos de uso"
+              feedback="Você tem que concordar antes de finalizar o cadastro"
+              feedbackType="invalid"
+            />
+          </Form.Group>
+          <Row className='mt-2 mb-2'>
+            <Col md={1}>
+              <Button id="botao" disabled={!temCategorias} type="submit">{props.modoEdicao ? "Alterar" : "Cadastrar"}</Button>
+
+            </Col>
+            <Col md={{ offset: 1 }} >
+              <Button onClick={() => {
+                props.setExibirTabela(true);
+                props.setModoEdicao(false);
+                zeraProduto();
+              }}>Voltar</Button>
+
+            </Col>
+          </Row>
+          <Toaster position='top-right' />
+        </Form>
+
+      );
+    }
 }
--- src/redux/produtoReducer.js
+++ src/redux/produtoReducer.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 import ESTADO from "./estados.js";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { consultarProduto,excluirProduto } from "../servicos/servicoProduto";
+import { consultarProduto,excluirProduto, gravarProduto, alterarProduto } from "../servicos/servicoProduto";
+
 
 
 export const buscarProdutos = createAsyncThunk('buscarProdutos', async () => {
@@ -38,7 +39,8 @@ export const apagarProduto= createAsyncThunk('apagarProduto', async (produto)=>{
     try {
         return {
             "status": resultado.status,
-            "mensagem": resultado.mensagem
+            "mensagem": resultado.mensagem,
+            "codigo":produto.codigo
         }
     } catch (erro) {
         return {
@@ -49,6 +51,66 @@ export const apagarProduto= createAsyncThunk('apagarProduto', async (produto)=>{
     }
 
 });
+
+export const incluirProduto = createAsyncThunk('incluirProduto', async (produto) =>{
+    try{
+
+        const resultado = await gravarProduto(produto);
+        if(resultado.status){
+            produto.codigo = resultado.codigo;
+            return {
+                "status": resultado.status,
+                "mensagem": resultado.mensagem,
+                //"codigo":produto.codigo
+                "produto":produto
+            }
+        }
+        else{
+             return {
+                "status": resultado.status,
+                "mensagem": resultado.mensagem,
+                //"codigo":produto.codigo
+            }
+        }
+    }
+    catch(erro){
+        return{
+            "status": false,
+           "mensagem": "Erro :" + erro.message,
+        }
+    }
+
+})
+export const atualizarProduto = createAsyncThunk('atualizarProduto', async (produto) =>{
+    try{
+
+        const resultado = await alterarProduto(produto);
+        if(resultado.status){
+            produto.codigo = resultado.codigo;
+            return {
+                "status": resultado.status,
+                "mensagem": resultado.mensagem,
+                //"codigo":produto.codigo
+                "produto":produto
+            }
+        }
+        else{
+             return {
+                "status": resultado.status,
+                "mensagem": resultado.mensagem,
+                //"codigo":produto.codigo
+            }
+        }
+    }
+    catch(erro){
+        return{
+            "status": false,
+           "mensagem": "Erro :" + erro.message,
+        }
+    }
+
+})
+
 const produtoReducer = createSlice({
     name: "produto",
     initialState: {
@@ -81,16 +143,62 @@ const produtoReducer = createSlice({
             })
             .addCase(apagarProduto.pending, (state, action)=>{
                 state.estado = ESTADO.PENDENTE
-                state.mensagem = action.payload.mensagem
+                state.mensagem = "Processando requisição (excluiindo o produto do backend)"
             })
             .addCase(apagarProduto.fulfilled, (state,action)=>{
-                state.estado=ESTADO.OCIOSO
                 state.mensagem= action.payload.mensagem
+                if(action.payload.status){
+                    
+                    state.estado=ESTADO.OCIOSO
+                    state.listaDeProdutos = state.listaDeProdutos.filter((item)=> item.codigo !== action.payload.codigo)
+                }else{
+                    state.estado=ESTADO.ERRO
+                }
                 //altera a lista de produto??
             })
             .addCase(apagarProduto.rejected, (state,action)=>{
                 state.estado=ESTADO.ERRO
                 //state.mensagem=action.payload.mensagem
+            })
+            .addCase(incluirProduto.pending,(state,action)=>{
+                state.estado=ESTADO.PENDENTE;
+                state.mensagem="Processando a requisição (inclusão do produto no backend)"
+            })
+            .addCase(incluirProduto.fulfilled,(state,action)=>{
+                if(action.payload.status){
+                    state.estado= ESTADO.OCIOSO;
+                    state.mensagem= action.payload.mensagem;
+                    state.listaDeProdutos.push(action.payload.produto)
+                }
+                else{
+                    state.estado = ESTADO.ERRO;
+                    state.mensagem= action.payload.mensagem;
+                }
+            })
+            .addCase(incluirProduto.rejected,(state,action)=>{
+                state.estado=ESTADO.ERRO;
+                state.mensagem= action.payload.mensagem;
+
+            })
+            .addCase(atualizarProduto.pending,(state,action)=>{
+                state.estado=ESTADO.PENDENTE;
+                state.mensagem="Processando a requisição (Atualização do produto no backend)"
+            })
+            .addCase(atualizarProduto.fulfilled,(state,action)=>{
+                if(action.payload.status){
+                    state.estado= ESTADO.OCIOSO;
+                    state.mensagem= action.payload.mensagem;
+                    state.listaDeProdutos = state.listaDeProdutos.map((item)=> item.codigo === action.payload.codigo ? action.payload.produto : item)
+                }
+                else{
+                    state.estado = ESTADO.ERRO;
+                    state.mensagem= action.payload.mensagem;
+                }
+            })
+            .addCase(atualizarProduto.rejected,(state,action)=>{
+                state.estado=ESTADO.ERRO;
+                state.mensagem= action.payload.mensagem;
+
             })
     }
 })
