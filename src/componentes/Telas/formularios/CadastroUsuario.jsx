c22496d580f6112813991f5e45d47b01d0dd968b
Joao-Paulo-Bortolocce <bbortolocce@gmail.com>
2024-12-06 02:07:41 -0300

    Finalizando o projeto

--- src/componentes/Telas/formularios/CadastroUsuario.jsx
+++ src/componentes/Telas/formularios/CadastroUsuario.jsx
@@ -3,21 +3,23 @@ import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
-import { InputGroup } from 'react-bootstrap';
+import { InputGroup, Container } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { incluirUsuario, atualizarUsuario } from "../../../redux/usuarioReducer";
 import { Alert, Spinner } from 'react-bootstrap';
 import ESTADO from '../../../redux/estados';
 import { useContext } from "react";
 import { ContextoUsuario } from "../../../App";
+import {toast, Toaster} from 'react-hot-toast';
+
 
 export default function CadastroUsuario(props) {
-  const { usuario , setUsuario} = useContext(ContextoUsuario)
+  const { usuario, setUsuario } = useContext(ContextoUsuario)
   const [validated, setValidated] = useState(false);
   const despachante = useDispatch();
   const { estado, mensagem } = useSelector((state) => state.usuario);
 
-  function limpaUser(){
+  function limpaUser() {
     props.setUsuario({
       "id": 0,
       "username": "",
@@ -34,15 +36,15 @@ export default function CadastroUsuario(props) {
     if (form.checkValidity()) {
       if (props.modoEdicao) {
         despachante(atualizarUsuario(props.usuario))
-        if(usuario.id===props.usuario.id){
+        if (usuario.id === props.usuario.id) {
 
           setUsuario({
-              "id": props.usuario.id,
-              "username": props.usuario.username,
-              "senha": props.usuario.senha,
-              "email": props.usuario.email,
-              "tipo": props.usuario.tipo,
-              "logado": true
+            "id": props.usuario.id,
+            "username": props.usuario.username,
+            "senha": props.usuario.senha,
+            "email": props.usuario.email,
+            "tipo": props.usuario.tipo,
+            "logado": true
           })
         }
       }
@@ -63,6 +65,21 @@ export default function CadastroUsuario(props) {
     const id = event.currentTarget.id;
     const valor = event.currentTarget.value;
     props.setUsuario({ ...props.usuario, [id]: valor })
+  }
+
+  function verificaAdm(event) {
+    const valor = event.currentTarget.value;
+    if (valor !== "admin") {
+      toast.error("Senha de Administrador informada esta incorreta!")
+      props.setUsuario({
+        "id": props.usuario.id,
+        "username": props.usuario.username,
+        "senha": props.usuario.senha,
+        "email": props.usuario.email,
+        "tipo": props.usuario.tipo,
+        "senhaAdmin":""
+      })
+    }
   }
 
   if (estado === ESTADO.PENDENTE) {
@@ -89,100 +106,104 @@ export default function CadastroUsuario(props) {
     else {
 
       return (
-        <Form noValidate validated={validated} onSubmit={handleSubmit} className='container'>
-          <Row className="mb-6">
-            <Form.Group as={Col} md="3" controlId="validationCustom05">
-              <Form.Label>ID</Form.Label>
-              <Form.Control type="text" required value={props.usuario.id} onChange={manipularMudanca} disabled={true} id="username" />
-
-            </Form.Group>
-            <Form.Group as={Col} md="3" controlId="validationCustom05">
-              <Form.Label>username</Form.Label>
-              <Form.Control type="text" required value={props.usuario.username} onChange={manipularMudanca} id="username" />
-              <Form.Control.Feedback type="invalid">
-                Por-Favor informe o username da usuario
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="5">
-              <Form.Label>Email</Form.Label>
-              <InputGroup hasValidation>
-                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                <Form.Control
-                  type="text"
-                  placeholder="E-mail"
-                  aria-describedby="inputGroupPrepend"
-                  required
-                  id="email"
-                  value={props.usuario.email}
-                  onChange={manipularMudanca}
-                />
+        <Container>
+          <Form noValidate validated={validated} onSubmit={handleSubmit} className='container'>
+            <Row className="mb-6">
+              <Form.Group as={Col} md="3" controlId="validationCustom05">
+                <Form.Label>ID</Form.Label>
+                <Form.Control type="text" required value={props.usuario.id} onChange={manipularMudanca} disabled={true} id="username" />
+              </Form.Group>
+              <Form.Group as={Col} md="3" controlId="validationCustom05">
+                <Form.Label>username</Form.Label>
+                <Form.Control type="text" required value={props.usuario.username} onChange={manipularMudanca} id="username" />
                 <Form.Control.Feedback type="invalid">
-                  Por-favor informe seu e-mail
+                  Por-Favor informe o username da usuario
                 </Form.Control.Feedback>
-              </InputGroup>
-            </Form.Group>
-          </Row>
-          <Row className="mb-6">
-            <Form.Group as={Col} md="4" controlId="validationCustom05">
-              <Form.Label>Senha</Form.Label>
-              <Form.Control type="password" required value={props.usuario.senha} onChange={manipularMudanca} id="senha" placeholder='******' />
-              <Form.Control.Feedback type="invalid">
-                Por-Favor informe a senha da usuario
-              </Form.Control.Feedback>
-            </Form.Group>
-            {props.usuario.tipo === "adm" ? <Form.Group as={Col} md="4" controlId="validationCustom01">
-              <Form.Label>Senha de Administrador</Form.Label>
-              <Form.Control
+              </Form.Group>
+              <Form.Group as={Col} md="5">
+                <Form.Label>Email</Form.Label>
+                <InputGroup hasValidation>
+                  <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                  <Form.Control
+                    type="text"
+                    placeholder="E-mail"
+                    aria-describedby="inputGroupPrepend"
+                    required
+                    id="email"
+                    value={props.usuario.email}
+                    onChange={manipularMudanca}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Por-favor informe seu e-mail
+                  </Form.Control.Feedback>
+                </InputGroup>
+              </Form.Group>
+            </Row>
+            <Row className="mb-6">
+              <Form.Group as={Col} md="4" controlId="validationCustom05">
+                <Form.Label>Senha</Form.Label>
+                <Form.Control type="password" required value={props.usuario.senha} onChange={manipularMudanca} id="senha" placeholder='******' />
+                <Form.Control.Feedback type="invalid">
+                  Por-Favor informe a senha da usuario
+                </Form.Control.Feedback>
+              </Form.Group>
+              {props.usuario.tipo === "adm" ? <Form.Group as={Col} md="4" controlId="validationCustom01">
+                <Form.Label>Senha de Administrador</Form.Label>
+                <Form.Control
+                  required
+                  type="password"
+                  value={props.usuario.senhaAdmin}
+                  onChange={manipularMudanca}
+                  disabled={usuario.tipo !== "adm"}
+                  id="senhaAdmin"
+                  placeholder="*******"
+                  onBlur={verificaAdm}
+                />
+                <Form.Control.Feedback type='invalid'>Informe a senha correta</Form.Control.Feedback>
+              </Form.Group> :
+                ""
+              }
+              <Form.Group as={Col} md="3">
+                <Form.Label>Tipo</Form.Label>
+                <Form.Select
+                  aria-label="tipo"
+                  required
+                  id="tipo"
+                  disabled={usuario.tipo !== "adm"}
+                  value={props.usuario.tipo}
+                  onChange={manipularMudanca}
+                >
+                  <option value="">Selecione um Tipo</option>
+                  <option value="adm">Administrador</option>
+                  <option value="grt">Gerente</option>
+                  <option value="nrm">Normal</option>
+                </Form.Select>
+                <Form.Control.Feedback type="invalid">
+                  Por-favor informe seu Tipo
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+            <Form.Group className="mb-3">
+              <Form.Check
                 required
-                type="password"
-                value={props.usuario.senhaAdmin}
-                onChange={manipularMudanca}
-                disabled={usuario.tipo!=="adm"}
-                id="senhaAdmin"
-                placeholder="*******"
+                label="Concordo com os termos de uso"
+                feedback="Você tem que concordar antes de finalizar o cadastro"
+                feedbackType="invalid"
               />
-              <Form.Control.Feedback type='invalid'>Informe a senha correta</Form.Control.Feedback>
-            </Form.Group>:
-            ""
-            }
-            <Form.Group as={Col} md="3">
-              <Form.Label>Tipo</Form.Label>
-              <Form.Select
-                aria-label="tipo"
-                required
-                id="tipo"
-                disabled={usuario.tipo!=="adm"}
-                value={props.usuario.tipo}
-                onChange={manipularMudanca}
-              >
-                <option value="">Selecione um Tipo</option>
-                <option value="adm">Administrador</option>
-                <option value="nrm">Normal</option>
-              </Form.Select>
-              <Form.Control.Feedback type="invalid">
-                Por-favor informe seu Tipo
-              </Form.Control.Feedback>
             </Form.Group>
-          </Row>
-          <Form.Group className="mb-3">
-            <Form.Check
-              required
-              label="Concordo com os termos de uso"
-              feedback="Você tem que concordar antes de finalizar o cadastro"
-              feedbackType="invalid"
-            />
-          </Form.Group>
-          <Row>
-
-            <Col md={1}><Button type="submit">{props.modoEdicao ? "Alterar" : "Cadastrar"}</Button></Col>
-            <Col md={{ offset: 1 }}>
-              <Button onClick={() => { props.setExibirTabela(true) 
-                props.setModoEdicao(false)
-                limpaUser()
-              }}>Voltar</Button>
-            </Col>
-          </Row>
-        </Form>
+            <Row>
+              <Col md={1}><Button type="submit">{props.modoEdicao ? "Alterar" : "Cadastrar"}</Button></Col>
+              <Col md={{ offset: 1 }}>
+                <Button onClick={() => {
+                  props.setExibirTabela(true)
+                  props.setModoEdicao(false)
+                  limpaUser()
+                }}>Voltar</Button>
+              </Col>
+            </Row>
+          </Form>
+            <Toaster position='top-right' />
+        </Container>
       );
     }
 }
